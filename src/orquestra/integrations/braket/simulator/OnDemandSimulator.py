3730fef08c4296d712a3ab4272fd6c35e7155e50
boniface316 <boniface.yogi@gmail.com>
2022-07-21 15:39:22 -0400

    build: on demand simulator is added

---
+++ src/orquestra/integrations/braket/simulator/OnDemandSimulator.py
@@ -0,0 +1,41 @@
+################################################################################
+# © Copyright 2021-2022 Zapata Computing Inc.
+################################################################################
+
+import boto3
+from braket.aws import AwsDevice
+from braket.aws.aws_session import AwsSession
+
+from ._base import BraketBasedSimulator
+
+
+class BraketOnDemandSimulator(BraketBasedSimulator):
+    supports_batching = False
+
+    def __init__(self, simulator: str, profile_name: str, noise_model=None):
+
+        aws_session = _get_aws_session(profile_name)
+
+        simulators = AwsDevice.get_devices(types=["SIMULATOR"], aws_session=aws_session)
+        if simulator not in [braket_simulator.name for braket_simulator in simulators]:
+            raise ValueError("The simulator provided is not a Braket Simulator")
+
+        if noise_model is None:
+            simulator = AwsDevice(_get_simulator_arn(simulator, aws_session))
+        else:
+            simulator = AwsDevice(_get_simulator_arn("dm1", aws_session))
+
+        super().__init__(simulator, noise_model)
+
+
+def _get_simulator_arn(name, aws_session):
+    simulator_properties = AwsDevice.get_devices(names=[name], aws_session=aws_session)[
+        0
+    ]
+
+    return simulator_properties.arn
+
+
+def _get_aws_session(profile_name):
+    boto_session = boto3.session.Session(profile_name=profile_name)
+    return AwsSession(boto_session)
