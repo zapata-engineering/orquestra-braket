85aeb7e9e42b8ac40e385bf42195ff6c7a6ba1fa
boniface316 <boniface.yogi@gmail.com>
2022-09-26 15:46:37 -0400

    fix: AWS_REGION is updated on the test to relect the correct variable name

--- tests/orquestra/braket/simulator/ondemandbraket_test.py
+++ tests/orquestra/braket/simulator/ondemandbraket_test.py
@@ -14,11 +14,11 @@ from orquestra.quantum.circuits import CNOT, Circuit, X
 from orquestra.integrations.braket.simulator import BraketOnDemandSimulator
 
 boto_session_type = os.environ["SESSION_TYPE"]
-region_name = os.environ["AWS_DEFAULT_REGION"]
 
 if boto_session_type == "mock":
     boto_session = Mock(spec=Session)
 else:
+    region_name = os.environ["AWS_DEFAULT_REGION"]
     boto_session = Session(region_name=region_name)
 
 
